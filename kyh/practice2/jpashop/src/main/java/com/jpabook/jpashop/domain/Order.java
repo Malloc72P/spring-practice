8fdbd67549894704f56dbe251533d061d3b73825
DESKTOP-UAFR4CS\scra <scra1028@gmail.com>
2021-09-24 09:54:00 +0900

    feat: 엔티티 클래스 구현
    - 강의를 듣고 JPA를 사용해서 연관관계를 매핑함
    - 아직 다 완성하지 못함

---
+++ kyh/practice2/jpashop/src/main/java/com/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,38 @@
+package com.jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")//order는 예약어라서 쓰면 안되니까 이렇게 정해줘야 함
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne//여러 주문은 하나의 멤버와 관계를 맺으므로 다대일
+    @JoinColumn(name = "member_id")//매핑을 어떤 컬럼으로 할지를 정하는 애너테이션
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery = new Delivery();
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+}
---
+++ kyh/practice2/jpashop/src/main/java/com/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,33 @@
+package com.jpabook.jpashop.domain.item;
+
+import com.jpabook.jpashop.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+public abstract class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany
+    @JoinTable(name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id")
+    )
+    private List<Category> categories = new ArrayList<>();
+}
