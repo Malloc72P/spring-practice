1b9379ae8fe4517f7a36a903f6bcbb4c226f1881
malloc72p <dase1102@gmail.com>
2021-09-24 15:21:01 +0900

    feat: 도메인 계층 기능구현
    - 연관관계를 LAZY 로딩으로 변경함
    - order의 orderItems에 cascade를 적용함

--- kyh/practice2/jpashop/src/main/java/com/jpabook/jpashop/domain/Order.java
+++ kyh/practice2/jpashop/src/main/java/com/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Table(name = "orders")//order는 예약어라서 쓰면 안되니까 이렇게 정해줘야 함
 @Getter
@@ -19,14 +21,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne//여러 주문은 하나의 멤버와 관계를 맺으므로 다대일
+    @ManyToOne(fetch = LAZY)//여러 주문은 하나의 멤버와 관계를 맺으므로 다대일
     @JoinColumn(name = "member_id")//매핑을 어떤 컬럼으로 할지를 정하는 애너테이션
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery = new Delivery();
 
@@ -35,4 +37,14 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
+    //연관관계 메서드
+    public void setMember(Member member) {
+        this.member = member;//자신의 필드에도 넣는다
+        member.getOrders().add(this);//연관관계의 상대측에도 자신의 객체를 넣는다
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
