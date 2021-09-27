042f7042b5f48a89c25fab357a3bfda135ee3995
malloc72p <dase1102@gmail.com>
2021-09-27 11:18:30 +0900

    feat: 상품 리포지토리 및 서비스 구현

--- kyh/practice2/jpashop/src/main/java/com/jpabook/jpashop/domain/item/Item.java
+++ kyh/practice2/jpashop/src/main/java/com/jpabook/jpashop/domain/item/Item.java
@@ -3,6 +3,7 @@ package com.jpabook.jpashop.domain.item;
 import com.jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.util.NestedServletException;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -30,4 +31,24 @@ public abstract class Item {
             inverseJoinColumns = @JoinColumn(name = "item_id")
     )
     private List<Category> categories = new ArrayList<>();
+
+    /*
+    * 재고수량 증가(stock 증가)
+    * */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /*
+     * 재고수량 감소
+     * 데이터를 갖고 있는 쪽에서 비즈니스 메서드까지 가지고 있는편이 좋다
+     * 핵심 비즈니스 로직을 엔티티에 넣은 것이다.
+     * */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
---
+++ kyh/practice2/jpashop/src/main/java/com/jpabook/jpashop/domain/item/ItemService.java
@@ -0,0 +1,31 @@
+package com.jpabook.jpashop.domain.item;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    /*
+    * 아이템 저장 메서드
+    * */
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
