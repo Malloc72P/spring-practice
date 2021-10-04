4171daf0a7d2fb73a4cc05b8349217c762c18bae
DESKTOP-UAFR4CS\scra <scra1028@gmail.com>
2021-10-04 18:39:46 +0900

    feat: 프론트 컨트롤러 - 사용성 개선
    - 컨트롤러에서 모델을 파라미터로 넘겨받도록 수정함
      뷰네임만 반환하도록 수정함

---
+++ kyh/spring-mvc-1/servlet/src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -0,0 +1,64 @@
+package hello.servlet.web.frontcontroller.v4;
+
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV4", urlPatterns = "/front-controller/v4/*")
+public class FrontControllerServletV4 extends HttpServlet {
+
+    private Map<String, ControllerV4> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV4() {
+        controllerMap.put("/front-controller/v4/members/new-form", new MemberFormControllerV4());
+        controllerMap.put("/front-controller/v4/members/save", new MemberSaveControllerV4());
+        controllerMap.put("/front-controller/v4/members", new MemberListControllerV4());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+        ControllerV4 controllerV4 = controllerMap.get(requestURI);
+        if (controllerV4 == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        Map<String, String> paramMap = createParamMap(request);
+        Map<String, Object> model = new HashMap<>();
+        String viewName = controllerV4.process(paramMap, model);
+
+        MyView myView = viewResolver(viewName);
+
+        myView.render(model, request, response);
+    }
+
+    /*
+     * 뷰 이름을 해결해줌
+     * 논리명을 물리명으로 바꿔줌
+     * */
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+    /*
+     * request에서 패러미터 싹 다 꺼내서 컨트롤러한테 넘길 패러미터 맵을 만들어줌
+     * */
+    private Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames()
+                .asIterator()
+                .forEachRemaining(key -> paramMap.put(key, request.getParameter(key)));
+        return paramMap;
+    }
+}
