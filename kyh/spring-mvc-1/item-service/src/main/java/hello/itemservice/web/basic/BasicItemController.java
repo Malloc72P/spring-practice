83e31346c47ad64190add06d4cb86aca36e3accc
malloc72p <dase1102@gmail.com>
2021-10-12 13:53:33 +0900

    feat: 아이템 컨트롤러 구현
    - 목록조회 컨트롤러 추가
    - 목록조회 뷰 생성

---
+++ kyh/spring-mvc-1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,48 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+/**
+ * 아이템 관련 요청을 처리하는 컨트롤러
+ *
+ * @author na seungchul
+ */
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    /**
+     * 아이템 목록조회요청을 처리하는 컨트롤러
+     *
+     * @param model 모델 - 아이템목록인 items가 담긴다
+     * @return 뷰 네임 - 아이템목록페이지
+     */
+    @GetMapping()
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /**
+     * 테스트를 위한 초기화 메서드.
+     * 더미데이터를 넣는다
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+}
