8e1bde8014a5f055f4793de1e71c9324cf1ef486
malloc72p <dase1102@gmail.com>
2021-10-12 14:03:59 +0900

    feat: 아이템 컨트롤러 - 아이템 상세보기 구현
    - 아이템 상세조회 컨트롤러 추가
    - 아이템 상세조회 뷰 생성

--- kyh/spring-mvc-1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ kyh/spring-mvc-1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -24,6 +25,16 @@ public class BasicItemController {
     private final ItemRepository itemRepository;
 
     /**
+     * 테스트를 위한 초기화 메서드.
+     * 더미데이터를 넣는다
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+    /**
      * 아이템 목록조회요청을 처리하는 컨트롤러
      *
      * @param model 모델 - 아이템목록인 items가 담긴다
@@ -36,13 +47,10 @@ public class BasicItemController {
         return "basic/items";
     }
 
-    /**
-     * 테스트를 위한 초기화 메서드.
-     * 더미데이터를 넣는다
-     */
-    @PostConstruct
-    public void init() {
-        itemRepository.save(new Item("itemA", 10000, 10));
-        itemRepository.save(new Item("itemB", 20000, 20));
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "/basic/item";
     }
 }
