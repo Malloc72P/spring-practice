c6da5d7e84fb6e8a46043f93e8799cafaa01ad9f
malloc72p <dase1102@gmail.com>
2021-10-13 18:16:49 +0900

    feat: 템플릿 레이아웃에 대해 학습했다

--- kyh/spring-mvc-2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ kyh/spring-mvc-2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -23,4 +23,24 @@ public class TemplateController {
         return "template/fragment/fragmentMain";
     }
 
+    /**
+     * 템플릿 레이아웃 예제에 대한 컨트롤러
+     *
+     * @return 템플릿 레이아웃 예제의 뷰 이름
+     */
+    @GetMapping("/layout")
+    public String layout() {
+        return "template/layout/layoutMain";
+    }
+
+    /**
+     * 템플릿 레이아웃을 확장한 예제에 대한 컨트롤러 <br>
+     * 모든 페이지의 레이아웃을 정한 경우에 대한 예제이다
+     *
+     * @return 해당 예제의 뷰 이름
+     */
+    @GetMapping("/layoutExtend")
+    public String layoutExtend() {
+        return "template/layoutExtend/layoutExtendMain";
+    }
 }
