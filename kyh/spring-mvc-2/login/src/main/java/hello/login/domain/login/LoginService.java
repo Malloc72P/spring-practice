a55e652dd8d668f43b77e28067c0063215ca5f98
DESKTOP-UAFR4CS\scra <scra1028@gmail.com>
2021-10-21 16:52:27 +0900

    feat: 쿠키를 사용해서 로그인 기능 구현

---
+++ kyh/spring-mvc-2/login/src/main/java/hello/login/domain/login/LoginService.java
@@ -0,0 +1,24 @@
+package hello.login.domain.login;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 로그인 기능
+     *
+     * @return null이면 로그인 실패
+     */
+    public Member Login(String loginId, String password) {
+        return memberRepository.findByLoginId(loginId)
+                .filter(member -> member.getPassword().equals(password))
+                .orElse(null);
+    }
+}
