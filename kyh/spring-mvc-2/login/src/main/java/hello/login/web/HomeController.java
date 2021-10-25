e2a473edebb4fbf4db81f8e4808ac7d79d033fcc
DESKTOP-UAFR4CS\scra <scra1028@gmail.com>
2021-10-26 01:40:18 +0900

    feat: 로그인 인증을 인터셉터를 사용하도록 수정

--- kyh/spring-mvc-2/login/src/main/java/hello/login/web/HomeController.java
+++ kyh/spring-mvc-2/login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentresolver.Login;
 import hello.login.web.session.SessionConst;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
@@ -57,7 +58,7 @@ public class HomeController {
         return "loginhome";
     }
 
-//    @GetMapping("/")
+    //    @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model) {
         //세션을 가져오되, 없으면 생성하지 않고 비로그인 홈으로 보낸다
         HttpSession session = request.getSession(false);
@@ -66,7 +67,7 @@ public class HomeController {
         }
 
         //세션에서 회원정보를 꺼낸다. 없으면 비로그인 홈으로 보낸다
-       Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
 
         if (loginMember == null) {
             return "home";
@@ -80,13 +81,24 @@ public class HomeController {
      * request에서 세션객체를 얻고, 세션에서 값을 꺼내는 작업도 복잡하다 <br>
      * 스프링은 @SessionAttribute를 제공해서 이 복잡함을 해결해준다 <br>
      * 이 기능은 세션을 생성하지 않는다.
+     *
      * @param loginMember 세션 어트리뷰트. 로그인 된 유저의 경우, null대신 회원정보가 있다.
-     * @param model 모델
+     * @param model       모델
      * @return 로그인 유저는 로그인버전 홈, 비 로그인 유저는 일반 홈
      */
-    @GetMapping("/")
-    public String homeLoginV3Spring(@SessionAttribute(name=SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
+//    @GetMapping("/")
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
                                     Model model) {
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginhome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3ArgumentResolver(@Login Member loginMember, Model model) {
         if (loginMember == null) {
             return "home";
         }
