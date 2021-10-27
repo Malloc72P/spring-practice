eaf18fc31b5d702b23c819aa4d7ff3f4f6cf1991
DESKTOP-UAFR4CS\scra <scra1028@gmail.com>
2021-10-27 16:10:47 +0900

    feat: ExceptionResolver로 예외 상태코드 변환을 함

---
+++ kyh/spring-mvc-2/exception/exception/src/main/java/hello/exception/resolver/MyHandlerExceptionResolver.java
@@ -0,0 +1,30 @@
+package hello.exception.resolver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+
+        log.info("resolveException");
+
+        try {
+            if (ex instanceof IllegalArgumentException) {
+                log.info("illegalArgumentException resolver to 400");
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+                return new ModelAndView();
+            }
+        } catch (IOException e) {
+            log.info("resolver exception!", e);
+        }
+        return null;
+    }
+}
