9d61154748a4beac35d0d73807c92751ce7c1116
DESKTOP-UAFR4CS\scra <scra1028@gmail.com>
2021-10-28 16:12:40 +0900

    feat: 스프링에 직접 만든 포매터 적용

--- kyh/spring-mvc-2/typeconverter/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
+++ kyh/spring-mvc-2/typeconverter/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -1,9 +1,8 @@
 package hello.typeconverter;
 
-import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
-import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,7 +15,9 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
 
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        registry.addFormatter(new MyNumberFormatter());
+
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
     }
 }
