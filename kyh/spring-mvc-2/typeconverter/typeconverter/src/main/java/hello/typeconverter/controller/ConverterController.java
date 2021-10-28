2dbba017f72912ccac06e54564a11534ad2ae367
DESKTOP-UAFR4CS\scra <scra1028@gmail.com>
2021-10-28 15:43:03 +0900

    feat: 컨버전 서비스를 타임리프에서 이용해보았다

---
+++ kyh/spring-mvc-2/typeconverter/typeconverter/src/main/java/hello/typeconverter/controller/ConverterController.java
@@ -0,0 +1,35 @@
+package hello.typeconverter.controller;
+
+import hello.typeconverter.type.IpPort;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/converter")
+public class ConverterController {
+    @GetMapping("/view")
+    public String converterView(Model model) {
+        model.addAttribute("number", 10000);
+        model.addAttribute("ipPort", new IpPort("127.0.0.1", 8080));
+        return "converter-view";
+    }
+
+    @GetMapping("/edit")
+    public String converterForm(Model model) {
+        IpPort ipPort = new IpPort("127.0.0.1", 8080);
+        Form form = new Form(ipPort);
+
+        model.addAttribute("form", form);
+        return "converter-form";
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Form {
+        private IpPort ipPort;
+    }
+}
