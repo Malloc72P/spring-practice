ac74661c515cd5feaf2309f6c7b88bb8c488d00d
DESKTOP-UAFR4CS\scra <scra1028@gmail.com>
2021-10-29 12:20:01 +0900

    feat: 요청객체로 파일 업로드 기능 처리

---
+++ kyh/spring-mvc-2/upload/upload/src/main/java/hello/upload/controller/ServletUploadControllerV1.java
@@ -0,0 +1,37 @@
+package hello.upload.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.util.Collection;
+
+@Slf4j
+@Controller
+@RequestMapping("/servlet/v1")
+public class ServletUploadControllerV1 {
+
+    @GetMapping("/upload")
+    public String newFile() {
+        return "upload-form";
+    }
+
+    @PostMapping("/upload")
+    public String saveFileV1(HttpServletRequest request) throws ServletException, IOException {
+        log.info("request = {}", request.getClass().toString());
+        String itemName = request.getParameter("itemName");
+        log.info("itemName = {}", itemName);
+
+        Collection<Part> parts = request.getParts();
+//        log.info("parts = {}", parts);
+        log.info("parts.size() = {}", (long) parts.size());
+        return "upload-form";
+    }
+}
+
