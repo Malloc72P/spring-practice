debbac5eaf9e4f012181c82c1a55947f43edb500
DESKTOP-UAFR4CS\scra <scra1028@gmail.com>
2021-11-18 23:15:22 +0900

    feat: JPQL에 대해 학습했다

--- kyh/jpa-programming-basic/chap04/src/main/java/hellojpa/JpaMain.java
+++ kyh/jpa-programming-basic/chap04/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class JpaMain {
@@ -15,35 +18,7 @@ public class JpaMain {
         transaction.begin();
 
         try {
-            Address address = new Address("home-city", "home-street", "12345");
 
-            Member member1 = new Member();
-            member1.setUsername("hello");
-            member1.setHomeAddress(address);
-
-            member1.getFavoriteFoods().add("치킨");
-            member1.getFavoriteFoods().add("피자");
-            member1.getFavoriteFoods().add("콜라");
-
-            member1.getAddressHistory().add(new Address("city1", "street1", "12345"));
-            member1.getAddressHistory().add(new Address("city2", "street2", "12345"));
-            member1.getAddressHistory().add(new Address("city3", "street3", "12345"));
-
-            em.persist(member1);
-
-            em.flush();
-            em.clear();
-
-            Member findMember = em.find(Member.class, member1.getId());
-            findMember.setHomeAddress(new Address("new-1", "st-1", "12121"));
-
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");
-
-            findMember.getAddressHistory().remove(new Address("city1", "street1", "12345"));
-            findMember.getAddressHistory().add(new Address("new-city1", "new-street1", "new-12345"));
-
-            em.persist(findMember);
 
             transaction.commit();
         } catch (Exception e) {
