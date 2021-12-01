7ef49b359022cf4b1aa94a9bc64c6673a1832bea
DESKTOP-UAFR4CS\scra <scra1028@gmail.com>
2021-12-01 16:03:33 +0900

    feat: QueryDSL프로젝트 생성, 세팅 완료

---
+++ kyh/spring-data-jpa-querydsl/querydsl/src/main/java/study/querydsl/entity/Hello.java
@@ -0,0 +1,32 @@
+package study.querydsl.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+public class Hello {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Hello hello = (Hello) o;
+        return Objects.equals(getId(), hello.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
